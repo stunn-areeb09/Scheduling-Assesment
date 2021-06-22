d9a5f8ba0f310bdcf5c4b0a75ed59634de5e43ad
Syed Areeb Wadood <areeb.wadood9@gmail.com>
2021-06-22 20:07:01 +0530

    two dbs added

--- routes/teacher.js
+++ routes/teacher.js
@@ -4,10 +4,10 @@ const Router = express.Router();
 const mysqlConnection = require("../connection");
 
 Router.get("/" , (req , res)=>{
-    mysqlConnection.query("select * from people" , (err ,rows, fields) => {
+    mysqlConnection.query("select * from `teacher details`" , (err ,rows, fields) => {
         if(!err)
         {
-            res.send(rows);
+            res.send(  {  rows : fields  } );
         }
         else
         {
